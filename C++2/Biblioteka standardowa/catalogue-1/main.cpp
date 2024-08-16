b84afb60ca687ed197cf55391d9e13cf4b7b6832
Jakub Zasadni <129856604+JakubZasadni@users.noreply.github.com>
2024-08-16 18:02:21 +0200

    Add files via upload

---
+++ C++2/Biblioteka standardowa/catalogue-1/main.cpp
@@ -0,0 +1,11 @@
+// FIXME (skel): Brak szkieletu.
+#include <cstdlib>
+#include <iostream>
+#include <functional>
+#include <string>
+
+#include "catalogue.hpp"
+
+int main() {
+    return EXIT_SUCCESS;
+}
